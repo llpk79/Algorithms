3edf7577e692ea50379be5f30f6752af313835a8
Paulus Kutrich <pkutrich@gmail.com>
2019-11-06 19:51:38 -0800

    MVP

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,17 +1,41 @@
 #!/usr/bin/python
-
 import sys
 
-def making_change(amount, denominations):
-  pass 
+
+def making_change(amount, denominations, cache=None):
+    """Iterative version."""
+    print(amount)
+    cache = {0: 1} if cache is None else cache
+    for coin in denominations:
+        for x in range(coin, amount + 1):
+            if x in cache:
+                cache[x] += cache[x - coin]
+            else:
+                cache[x] = cache[x - coin]
+    return cache[amount]
+
+
+def making_change(amount, denominations, cache=None):
+    """Recursive version. Half as fast."""
+    cache = {0: 1} if cache is None else cache
+    if amount in cache:
+        return cache[amount]
+    for coin in denominations:
+        for x in range(coin, amount + 1):
+            if x in cache:
+                cache[x] += making_change(x - coin, denominations, cache)
+            else:
+                cache[x] = making_change(x - coin, denominations, cache)
+    return cache[amount]
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations),
+                                                                     amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python
-
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    if len(recipe) > len(ingredients):
+        return 0
+    max_ = math.inf
+    for rec, ing in zip(recipe.values(), ingredients.values()):
+        count = ing // rec
+        if count < max_:
+            max_ = count
+    return max_
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}."
+          .format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
