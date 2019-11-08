5598ebfe590e6275074cd7991b1a625fe82292ca
Paulus Kutrich <pkutrich@gmail.com>
2019-11-07 16:54:56 -0800

    add rps solution ;

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
-
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    plays = ['rock', 'paper', 'scissors']
+    return _rock_paper_scissors(n, plays, [])
+
+
+def _rock_paper_scissors(n, plays, answer=None, final=None):
+    final = [] if final is None else final
+    if n == 0:
+        final.append(answer)
+        return [[]]
+    for play in plays:
+        new_ans = answer + [play]
+        _rock_paper_scissors(n - 1, plays, new_ans, final)
+    return final
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print(rock_paper_scissors(4))
